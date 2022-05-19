a86e95c6ff58e2c18de9a4f997b00bd167203a0c
Glareone <enlarius@gmail.com>
2022-05-19 11:37:58 +0300

    deleted commented code

--- grpcServer/grpcServer/Services/HistoricalFeedService.cs
+++ grpcServer/grpcServer/Services/HistoricalFeedService.cs
@@ -2,9 +2,7 @@ using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
-using Google.Protobuf.WellKnownTypes;
 using GrpcServer;
-using Microsoft.AspNetCore.WebUtilities;
 
 namespace grpcServer.Services
 {
@@ -56,18 +54,6 @@ namespace grpcServer.Services
                 await responseStream.WriteAsync(historicalMessage);
                 i++;
             }
-
-            // return new Task(Task.FromResult(new HistoricalReply
-            // {
-            //     Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(),
-            //     Open = GetPseudoDoubleWithinRange(32000, 38000),
-            //     Close = GetPseudoDoubleWithinRange(32000, 36000),
-            //     Low = GetPseudoDoubleWithinRange(32000, 34000),
-            //     High = GetPseudoDoubleWithinRange(38000, 40000),
-            //     VolumeBtc = GetPseudoDoubleWithinRange(1, 1000),
-            //     VolumeCur = GetPseudoDoubleWithinRange(5, 5000),
-            //     WeightedPrice = GetPseudoDoubleWithinRange(35000, 35300)
-            // }));
         }
     }
 }
